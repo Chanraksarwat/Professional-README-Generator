fdecdbef23464a27ba1c3a1e9f8a4654d2d9f87e
Chanraksar Wat <chanraksarwat@gmail.com>
2021-04-12 21:24:46 -0500

    added more fucntion to generate answer and alert user to index.js

--- index.js
+++ index.js
@@ -58,5 +58,63 @@ function promptUser() {
           message: "Enter your github username"
         }
       ]);
-    }
+}
+
+// function that layout the ReadMe File
+function generateREADME(answers) {
+  return `# ${answers.project_title}
+    
+#### Table of Contents
+1. [Project Description](#project-description)
+2. [Installation Instructions](#installation-instructions)
+3. [Usage Information](#usage-information)
+4. [Contributor Guidelines](#contributor-guidelines)
+5. [Code of Conduct](#code-of-conduct)
+6. [Test Instructions](#test-instructions)
+7. [License](#license)
+8. [Questions](#questions)
+
+
+## Project Description
+* ${answers.description}
+
+## Installation Instructions
+* ${answers.install}
+
+## Usage Information
+* ${answers.use}
+
+## Contributor Guidelines
+* ${answers.contributions}
+
+## Code of Conduct
+* [Contributor Covenant Code of Conduct](https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.md)
+
+## Test Instructions
+* ${answers.test}
+
+## License
+* licensed under the ${answers.license}
+
+## Questions
+* For additional help or questions about collaboration, please reach out to ${answers.email}
+
+* Follow me on Github at [${answers.github}](http://github.com/${answers.github})`;
+  
+}
+
+// Alert prompt to user when done going through questions
+promptUser()
+  .then(function(answers) {
+    const readme = generateREADME(answers);
+
+ 
+    return writeFileAsync("README.md", readme);
+  })
+  .then(function() {
+    console.log(" README.md has been created!");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
