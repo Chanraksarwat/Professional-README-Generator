c8fdf55d7db47cc5c2c470e8e65a9fa6b22d6805
Chanraksar Wat <chanraksarwat@gmail.com>
2021-04-12 21:22:30 -0500

    added function to loop throgh questions to index.js

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
+//import files using require method
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
+
+// function to loop through quesitions
+function promptUser() {
+    
+  return inquirer.prompt([
+      {
+          type: "input",
+          name: "project_title",
+          message: "What is your project tittled?"
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Briefly describe your project"
+        },
+        {
+          type: "input",
+          name: "install",
+          message: "Are there any installations required?"
+        },
+        {
+          type: "input",
+          name: "use",
+          message: "What is the use of the application"
+        },
+        {
+          type: "input",
+          name: "contributions",
+          message: "Are there any contribution rules?"
+        },
+        {
+          type: "input",
+          name: "test",
+          message: "Please provide test instructions if applicable"
+        },
+        {
+          type: "checkbox",
+          message: "License?",
+          name: "license",
+          choices: [
+            "[MIT License](LICENSE.txt)", 
+            "[GNU GPLv3 License](COPYING.txt)", 
+          ]
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Enter your email account"
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "Enter your github username"
+        }
+      ]);
+    }
+
